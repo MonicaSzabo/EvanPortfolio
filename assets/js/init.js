b5467e08d451ff29c7fd97b5c93d9e09bd95e25c
Monica Szabo <monicaszb@gmail.com>
2017-01-26 22:19:19 -0600

    added rick page

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,5 +4,23 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    $(".work-menu").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#work").offset().top - 30
+      }, 900);
+    });
+
+    $(".about-menu").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#about").offset().top - 30
+      }, 900);
+    });
+
+    $(".contact-menu").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#contact").offset().top
+      }, 900);
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
