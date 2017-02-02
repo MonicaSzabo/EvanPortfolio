2d0b41885cf9183c08f848ab5097cfda41d22b9b
Monica Szabo <monicaszb@gmail.com>
2017-02-01 22:01:33 -0600

    added galleries

--- assets/js/init.js
+++ assets/js/init.js
@@ -22,5 +22,15 @@
       }, 900);
     });
 
+    jQuery("#gallery").unitegallery({
+      tile_enable_shadow:false,
+      tile_enable_border:true,
+      tiles_space_between_cols:15,
+      tiles_justified_space_between:15,
+      tiles_col_width:235,
+      tile_border_color: "#ffffff",
+      tile_enable_outline:true,
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
